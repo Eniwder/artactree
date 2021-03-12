8ac0aabeee7309cc4e3777d7a2afa7129fe0ebc6
Eniwder <akabudou0120@yahoo.co.jp>
2021-03-13 06:57:15 +0900

    'electron追加'

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,22 @@ module.exports = {
   transpileDependencies: [
     'vuetify',
   ],
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        productName: "artactree",
+        appId: "eniel.artactree",
+        win: {
+          icon: 'src/assets/icon.ico',
+          target: [
+            {
+              target: 'portable', // 'zip', 'nsis', 'portable'
+              arch: ['x64'] // 'x64', 'ia32'
+            }
+          ]
+        }
+      }
+    }
+  },
   publicPath: './'
 }
